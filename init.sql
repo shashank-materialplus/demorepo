a1055518b96e40ebeeb04e6a977c1c8cac0ecfd7
shashank.material <shashank.agrawal@materialplus.io>
2025-06-04 09:56:53 +0530

    pgres change

--- init.sql
+++ init.sql
@@ -1,2 +1,19 @@
-CREATE DATABASE IF NOT EXISTS springbootmicroservicesjwtproduct;
-CREATE DATABASE IF NOT EXISTS springbootmicroservicesjwtuser;
+DO $$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_database WHERE datname = 'springbootmicroservicesjwtproduct'
+   ) THEN
+      CREATE DATABASE springbootmicroservicesjwtproduct;
+   END IF;
+END
+$$;
+
+DO $$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_database WHERE datname = 'springbootmicroservicesjwtuser'
+   ) THEN
+      CREATE DATABASE springbootmicroservicesjwtuser;
+   END IF;
+END
+$$;
